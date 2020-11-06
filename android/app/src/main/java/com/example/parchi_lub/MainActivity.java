27f7f082c25b542af9675c3d9cfdb786d01a7c1b
kartik-pant-23 <pantkartik23@gmail.com>
2020-11-07 00:31:32 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/parchi_lub/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.parchi_lub;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Parchi Lub',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
